28f350656a8ae04a87dfdc02aff2b05b094a378d
Buck DeFore <bdefore@gmail.com>
2015-07-14 12:47:28 -0400

    done

--- house/lib/house.rb
+++ house/lib/house.rb
@@ -1,3 +1,32 @@
 class House
 
+  LINES = [
+    "This is the house that Jack built.\n",
+    "This is the malt that lay in the house that Jack built.\n",
+    "This is the rat that ate the malt that lay in the house that Jack built.\n",
+    "This is the cat that killed the rat that ate the malt that lay in the house that Jack built.\n",
+    "This is the dog that worried the cat that killed the rat that ate the malt that lay in the house that Jack built.\n",
+    "This is the cow with the crumpled horn that tossed the dog that worried the cat that killed the rat that ate the malt that lay in the house that Jack built.\n",
+    "This is the maiden all forlorn that milked the cow with the crumpled horn that tossed the dog that worried the cat that killed the rat that ate the malt that lay in the house that Jack built.\n",
+    "This is the man all tattered and torn that kissed the maiden all forlorn that milked the cow with the crumpled horn that tossed the dog that worried the cat that killed the rat that ate the malt that lay in the house that Jack built.\n",
+    "This is the priest all shaven and shorn that married the man all tattered and torn that kissed the maiden all forlorn that milked the cow with the crumpled horn that tossed the dog that worried the cat that killed the rat that ate the malt that lay in the house that Jack built.\n",
+    "This is the rooster that crowed in the morn that woke the priest all shaven and shorn that married the man all tattered and torn that kissed the maiden all forlorn that milked the cow with the crumpled horn that tossed the dog that worried the cat that killed the rat that ate the malt that lay in the house that Jack built.\n",
+    "This is the farmer sowing his corn that kept the rooster that crowed in the morn that woke the priest all shaven and shorn that married the man all tattered and torn that kissed the maiden all forlorn that milked the cow with the crumpled horn that tossed the dog that worried the cat that killed the rat that ate the malt that lay in the house that Jack built.\n",
+    "This is the horse and the hound and the horn that jkdkbelonged to the farmer sowing his corn that kept the rooster that crowed in the morn that woke the priest all shaven and shorn that married the man all tattered and torn that kissed the maiden all forlorn that milked the cow with the crumpled horn that tossed the dog that worried the cat that killed the rat that ate the malt that lay in the house that Jack built.\n"
+  ]
+
+  def line(index)
+    LINES[index-1]
+  end
+
+  def line_cool_way(index)
+    # from 0 to index
+    # build a string with LINES[index]
+    # but remove 'this is'
+  end
+
+  def recite
+    LINES.join.chomp
+  end
+
 end
