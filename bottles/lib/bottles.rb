57c8e034a7ef8cb2d255017ea3821d38f7652b6c
Max Miller <maxwellgmiller@gmail.com>
2015-07-14 09:42:24 -0400

    Add method for final verse

--- bottles/lib/bottles.rb
+++ bottles/lib/bottles.rb
@@ -2,10 +2,18 @@ class Bottles
   def verse(number)
     return penultimate_verse if number == 1
 
-    "#{ number } #{ pluralize "bottle", number } of beer on the wall, " +
-    "#{number} #{ pluralize "bottle", number } of beer.\n" +
-    "Take one down and pass it around, " +
-    "#{ number-1 } #{ pluralize "bottle", number-1 } of beer on the wall.\n"
+  def verse(number)
+    case number
+    when 0
+      final_verse
+    when 1
+      penultimate_verse
+    else
+      "#{ number } #{ pluralize "bottle", number } of beer on the wall, " +
+      "#{number} #{ pluralize "bottle", number } of beer.\n" +
+      "Take one down and pass it around, " +
+      "#{ number-1 } #{ pluralize "bottle", number-1 } of beer on the wall.\n"
+    end
   end
 
   private
@@ -15,6 +23,13 @@ class Bottles
     "1 bottle of beer.\n" +
     "Take it down and pass it around, " +
     "no more bottles of beer on the wall.\n"
+  end
+
+  def final_verse
+    "No more bottles of beer on the wall, " +
+    "no more bottles of beer.\n" +
+    "Go to the store and buy some more, " +
+    "99 bottles of beer on the wall.\n"
   end
 
   def pluralize(word, number)
