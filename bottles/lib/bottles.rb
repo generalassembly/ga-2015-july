3d1e1c99c7f6c2112a711ad5b50293522a1e3a26
Max Miller <maxwellgmiller@gmail.com>
2015-07-14 09:30:20 -0400

    3rd test PASSING

--- bottles/lib/bottles.rb
+++ bottles/lib/bottles.rb
@@ -1,5 +1,15 @@
 class Bottles
   def verse(number)
-    "#{number} bottles of beer on the wall, #{number} bottles of beer.\nTake one down and pass it around, #{number -1} bottles of beer on the wall.\n"
+    "#{ number } #{ pluralize "bottle", number } of beer on the wall, #{number} #{ pluralize "bottle", number } of beer.\nTake one down and pass it around, #{ number-1 } #{ pluralize "bottle", number-1 } of beer on the wall.\n"
+  end
+
+  private
+
+  def pluralize(word, number)
+    if number == 1
+      word
+    else
+      word + "s"
+    end
   end
 end
