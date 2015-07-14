043230613b60399513f667d737c073937886c878
Brooks Swinnerton <bswinnerton@gmail.com>
2015-07-14 14:03:07 -0400

    Further refactors

--- house/lib/house.rb
+++ house/lib/house.rb
@@ -1,27 +1,30 @@
 class House
+  PHRASES = [
+    'the horse and the hound and the horn that belonged to',
+    'the farmer sowing his corn that kept',
+    'the rooster that crowed in the morn that woke',
+    'the priest all shaven and shorn that married',
+    'the man all tattered and torn that kissed',
+    'the maiden all forlorn that milked',
+    'the cow with the crumpled horn that tossed',
+    'the dog that worried',
+    'the cat that killed',
+    'the rat that ate',
+    'the malt that lay in',
+    'the house that Jack built.',
+  ]
 
   def recite
-    1.upto(12).collect {|i| line(i)}.join("\n")
+    1.upto(PHRASES.size).map { |i| line(i) }.join("\n")
   end
 
   def line(num)
-    "This is #{phrase(num)}the house that Jack built.\n"
+    "This is #{phrase(num)}\n"
   end
 
+  private
+
   def phrase(line_number)
-    [
-      'the horse and the hound and the horn that belonged to ',
-      'the farmer sowing his corn that kept ',
-      'the rooster that crowed in the morn that woke ',
-      'the priest all shaven and shorn that married ',
-      'the man all tattered and torn that kissed ',
-      'the maiden all forlorn that milked ',
-      'the cow with the crumpled horn that tossed ',
-      'the dog that worried ',
-      'the cat that killed ',
-      'the rat that ate ',
-      'the malt that lay in ',
-      ''
-    ].last(line_number).join
+    PHRASES.last(line_number).join(' ')
   end
 end
