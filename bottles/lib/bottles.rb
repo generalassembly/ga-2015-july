f4ad010587259c07dbef1d3066d8c2ef4b1da6fd
Max Miller <maxwellgmiller@gmail.com>
2015-07-14 09:24:10 -0400

    first test passing

--- bottles/lib/bottles.rb
+++ bottles/lib/bottles.rb
@@ -1,3 +1,5 @@
 class Bottles
-
+  def verse(number)
+    "#{number} bottles of beer on the wall, #{number} bottles of beer.\nTake one down and pass it around, #{number -1} bottles of beer on the wall.\n"
+  end
 end
